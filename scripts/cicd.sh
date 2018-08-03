34f3eed598eedb0bead0daba30058d3717664e03
Longfei Ren <loren@redhat.com>
2018-08-03 15:30:24 +0800

    add path

--- scripts/cicd.sh
+++ scripts/cicd.sh
@@ -2,4 +2,4 @@
 
 PWD=`pwd`
 
-ansible-playbook -i $PWD/resources/container-pipelines/applier/inventory/ $PWD/resources/openshift-applier/playbooks/openshift-cluster-seed.yml
+ansible-playbook -i $PWD/resources/container-pipelines/basic-spring-boot/applier/inventory/ $PWD/resources/openshift-applier/playbooks/openshift-cluster-seed.yml
